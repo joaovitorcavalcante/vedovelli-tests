5a0d4f5685ea612472552e4390e2cc24a41defc1
JOAO VITOR CAVALCANTE BARBOSA <117310@pmenos.com.br>
2022-01-18 19:34:41 -0300

    initial commit

---
+++ projeto01/src/lib/queryString.spec.js
@@ -0,0 +1,35 @@
+const { queryString } = require('./queryString');
+
+describe('Object to query string', () => {
+  it('should create a valid query string when an object is provided', () => {
+    const obj = {
+      name: 'Fabio',
+      profession: 'developer',
+    };
+
+    expect(queryString(obj)).toBe('name=Fabio&profession=developer');
+  });
+
+  it('should create a valid query string even then an array is passed as value', () => {
+    const obj = {
+      name: 'Fabio',
+      abilities: ['JS', 'TDD'],
+    };
+
+    expect(queryString(obj)).toBe('name=Fabio&abilities=JS,TDD');
+  });
+
+  it('should throw an error when an object is passed as value', () => {
+    const obj = {
+      name: 'Fabio',
+      abilities: {
+        first: 'JS',
+        second: 'TDD',
+      },
+    };
+
+    expect(() => {
+      queryString(obj);
+    }).toThrowError();
+  });
+});
